1177ffaf70925a07321fc371eb08fe847622e86f
Arthur Santos <arthursa1998@gmail.com>
2024-07-20 08:50:38 -0300

    feature: create producer entity, service, controller and some dtos

--- src/main/java/stock/product/service/controller/ProducerController.java
+++ src/main/java/stock/product/service/controller/ProducerController.java
@@ -1,10 +1,15 @@
 package stock.product.service.controller;
 
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stock.product.service.models.dto.producer.ProducerDTO;
 import stock.product.service.service.ProducerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("producer")
@@ -12,4 +17,14 @@ import stock.product.service.service.ProducerService;
 public class ProducerController {
     private final ProducerService producerService;
 
+   @PostMapping
+   public ResponseEntity registerProducer(@RequestBody @Valid ProducerDTO producerDTO) {
+        producerService.registerProducer(producerDTO);
+       return ResponseEntity.status(HttpStatus.CREATED).build();
+   }
+
+   @GetMapping
+   public ResponseEntity<List<ProducerDTO>> getAllProducers() {
+       return ResponseEntity.ok(producerService.getAll());
+   }
 }
